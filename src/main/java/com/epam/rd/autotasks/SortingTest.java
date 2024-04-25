4f01f19b5d3214154ef1c96e45f4a94f2f42ae0d
Mariam Akiashvili <mari.akiashvili@gmail.com>
2024-04-25 04:57:24 +0400

    Commit V3

--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -3,51 +3,97 @@ package com.epam.rd.autotasks;
 import org.junit.Assert;
 import org.junit.Test;
 
-import org.junit.jupiter.api.Assertions;
-
 import java.util.Arrays;
+
+
+//public class SortingTest {
+//
+//    Sorting sorting = new Sorting();
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testSortNullArray() {
+//
+//        sorting.sort(null);
+//    }
+//
+//
+//
+//    @Test
+//    public void testEmptyCase(){
+//        int[] arr =  {};
+//        sorting.sort(arr);
+//        Assert.assertEquals(0, arr.length);
+//    }
+//
+//    @Test
+//    public void testSingleElementArrayCase() {
+//        int[]arr = new int[] {1};
+//        sorting.sort(arr);
+//        Assert.assertEquals(1, arr[0]);
+//    }
+//
+//    @Test
+//    public void testSortedArraysCase() {
+//        int [] arr2 = new int[] {1,1,3,4,9,22};
+//        sorting.sort(arr2);
+//        Assert.assertEquals(1, arr2[0]);
+//    }
+//
+//    @Test
+//    public void testReversedSorted() {
+//        int[] arr = new int[] {2,2,1,4,12};
+//
+//        sorting.sort(arr);
+//        Assert.assertEquals(1, arr[0]);
+//    }
+//
+//
+//
+//}
+//
+//
+//
+//
 
 
 public class SortingTest {
 
     Sorting sorting = new Sorting();
 
-    @Test
-    public void testSortNullArray() {
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> sorting.sort(null));
+    @Test (expected = IllegalArgumentException.class)
+    public void testNullCase(){
+        sorting.sort(null);
     }
-
-
 
     @Test
     public void testEmptyCase(){
-        int[] arr =  {};
+        int [] arr = new int[]{};
         sorting.sort(arr);
-        Assert.assertEquals(0, arr.length);
+        Assert.assertArrayEquals(new int[] {}, arr);
+
     }
 
     @Test
     public void testSingleElementArrayCase() {
-        int[]arr = new int[] {1};
+        int[] arr = new int[]{4};
         sorting.sort(arr);
-        Assertions.assertEquals(1, arr[0]);
+        Assert.assertEquals(4, arr[0]);
     }
 
     @Test
     public void testSortedArraysCase() {
-        int [] arr2 = new int[] {1,1,3,4,9,22};
-        sorting.sort(arr2);
-        Assert.assertEquals(1, arr2[0]);
+        int[] arr = new int[]{1,2,3,4,5};
+        sorting.sort(arr);
+        Assert.assertArrayEquals(new int[]{1,2,3,4,5}, arr);
+//        Assert.assertNotNull(arr);
     }
 
-    @Test
+    @Test(expected = Exception.class)
     public void testOtherCases() {
-        int[] arr = new int[] {2,2,1,4,12};
 
-        sorting.sort(arr);
-//        arr[0] = 4;
-        Assert.assertEquals(1, arr[0]);
+        int[] arr2 = new int[]{1,2,3};
+        arr2 = null;
+        sorting.sort(arr2);
     }
 
 }
